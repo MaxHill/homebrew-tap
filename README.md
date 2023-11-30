bc2c130347d9d26e7f3e450f7f1134a3a4df4b55
Max Hill <max@hilloco.se>
2023-11-30 11:42:14 +0100

    feat: new pipeline

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homebrew-tap
+
+
+# Replace homepage
+sed -i 's/\(url.*"\).*\("\)/\1new_thing\2' input.txt
+sed -i 's/\(sha256.*"\).*\("\)/\1new_thing\2' input.txt
+sed -i 's/\(version.*"\).*\("\)/\1new_thing\2' input.txt
--- notes-cli.rb
+++ notes-cli.rb
@@ -1,9 +1,10 @@
 class NotesCli < Formula
   desc "Plain text notes cli framework"
   homepage "https://github.com/MaxHill/cli-notes"
-  url "https://github.com/MaxHill/cli-notes/releases/download/v0.15.0/notes-cli.tar.gz"
+  url "https://github.com/MaxHill/cli-notes/releases/download/v0.16.0/notes-cli.tar.gz"
   sha256 "1aebbacdc70b2f62b0e797f908be22a31914dee623fc53764bb9c3beba0b1cfc"
   license "MIT"
+  version "v0.0.1"
 
   def install
     bin.install "notes-cli"
