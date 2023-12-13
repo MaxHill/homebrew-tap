043bef8ad626d28404543b65b9f4a5eb6c86c301
Max Hill <max@hilloco.se>
2023-12-13 21:56:12 +0000

    get-calendar-events version v0.1.0

--- get-calendar-events.rb
+++ get-calendar-events.rb
@@ -1,10 +1,10 @@
 class GetCalendarEvents < Formula
   desc "Prints calendar event between two dates as json"
   homepage "https://github.com/MaxHill/get-calendar-events"
-  url "https://github.com/MaxHill/get-calendar-events/releases/download/v0.0.1/get-calendar-events.tar.gz"
-  sha256 "1aebbacdc70b2f62b0e797f908be22a31914dee623fc53764bb9c3beba0b1cfc"
+  url "https://github.com/MaxHill/get-calendar-events/releases/download/v0.1.0/get-calendar-events.tar.gz"
+  sha256 "07dc64600b603afdc84f536d114f7a4f945e52f5c340aaeae866ffd1df4d1efb"
   license "MIT"
-  version "v0.0.1"
+  version "v0.1.0"
 
   def install
     bin.install "get-calendar-events"
