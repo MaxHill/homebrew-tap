9e6dd0a7909baaa3730c527aa14c96f9e99eb487
Max Hill <max@hilloco.se>
2024-06-11 21:13:55 +0000

    notes-cli version v0.4.0

--- notes-cli.rb
+++ notes-cli.rb
@@ -1,10 +1,10 @@
 class NotesCli < Formula
   desc "Plain text notes cli framework"
   homepage "https://github.com/MaxHill/cli-notes"
-  url "https://github.com/MaxHill/cli-notes/releases/download/v0.16.0/notes-cli.tar.gz"
-  sha256 "1aebbacdc70b2f62b0e797f908be22a31914dee623fc53764bb9c3beba0b1cfc"
+  url "https://github.com/MaxHill/cli-notes/releases/download/v0.4.0/notes-cli.tar.gz"
+  sha256 "16a8ccf201126954a46625c7d1292ee329f8b09e858873e4d5d3deb7a8de0c1c"
   license "MIT"
-  version "v0.0.1"
+  version "v0.4.0"
 
   def install
     bin.install "notes-cli"
